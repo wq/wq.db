35642fe6a3b91c98ac312803dbc4e41382abee2d
S. Andrew Sheppard <andrew@wq.io>
2012-06-25 02:31:35 +0000

    generic File and Image models

---
+++ files/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+import magic
+
+from wq.db.annotate.models import AnnotatedModel
+from wq.db.relate.models import RelatedModel
+
+class FileType(models.Model):
+    name     = models.CharField(max_length=255)
+    mimetype = models.CharField(max_length=255)
+    def __unicode__(self):
+        return '%s (%s)' % (self.name, self.mimetype)
+
+class BaseFile(AnnotatedModel, RelatedModel):
+    name = models.CharField(max_length=255, null=True, blank=True)     
+    type = models.ForeignKey(FileType, null=True, blank=True)
+
+    @property
+    def mimetype(self):
+        if self.file is not None:
+            mime = magic.Magic(mime=True)
+            return mime.from_file(self.file.path)
+        else:
+            return None
+
+    def save(self):
+        super(BaseFile, self).save()
+        if self.type is not None:
+            ftype, isnew = FileType.objects.get_or_create(mimetype=self.mimetype)
+            self.type = ftype
+            super(BaseFile, self).save()
+        if self.name is None or self.name == '':
+            udir = self._meta.get_field_by_name('file')[0].upload_to + '/'
+            self.name = self.file.name.replace(udir, '')
+            super(BaseFile, self).save()
+
+    def __unicode__(self):
+        return self.name
+
+class File(BaseFile):
+    file = models.FileField(upload_to='files')
+
+class Image(BaseFile):
+    file   = models.ImageField(upload_to='images', width_field='width', height_field='height')
+    width  = models.IntegerField(null=True, blank=True)
+    height = models.IntegerField(null=True, blank=True)
--- models.py
+++ models.py
@@ -2,3 +2,4 @@ from wq.db.annotate.models import *
 from wq.db.identify.models import *
 from wq.db.locate.models   import *
 from wq.db.relate.models   import *
+from wq.db.files.models    import *
