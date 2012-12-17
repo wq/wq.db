cf9ed5b2cf1f6d556044d81f2947de99701c26fa
S. Andrew Sheppard <andrew@wq.io>
2012-12-17 17:35:39 -0600

    simplify the common use of IdentifiedModel+RelatedModel

--- models.py
+++ models.py
@@ -3,3 +3,11 @@ from wq.db.identify.models import *
 from wq.db.locate.models   import *
 from wq.db.relate.models   import *
 from wq.db.files.models    import *
+
+class IdentifiedRelatedModelManager(IdentifiedModelManager, RelatedModelManager):
+    pass
+
+class IdentifiedRelatedModel(IdentifiedModel, RelatedModel):
+    objects = IdentifiedRelatedModelManager()
+    class Meta:
+        abstract = True
