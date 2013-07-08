802dec8fe51cc7a8595a90a5cd6d425af2883358
S. Andrew Sheppard <andrew@wq.io>
2013-07-07 21:56:23 -0500

    look for split date/time

--- patterns/base/models.py
+++ patterns/base/models.py
@@ -140,6 +140,7 @@ class NaturalKeyModel(models.Model):
 
 def extract_nested_key(key, cls, prefix=''):
     nested_key = cls.get_natural_key_fields()
+    local_fields = {field.name: field for field in cls._meta.local_fields}
     values = []
     has_val = False
     if prefix:
@@ -149,6 +150,13 @@ def extract_nested_key(key, cls, prefix=''):
         if not val and nname.endswith('__primary_identifiers__slug'):
             nname = nname.replace('__primary_identifiers__slug', '')
             val = key.pop(prefix + nname, None)
+        if not val and nname in local_fields:
+            if type(local_fields[nname]).__name__ == 'DateTimeField':
+                date = key.pop(nname + '_date', None)
+                time = key.pop(nname + '_time', None)
+                if date and time:
+                    val = '%s %s' % (date, time)
+            
         if val is not None and val != '':
             has_val = True
         values.append(val)
