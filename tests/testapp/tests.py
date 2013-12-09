20de96b020777247404cf969c3ac8d8ddb6d5ff4
S. Andrew Sheppard <andrew@wq.io>
2013-12-08 18:02:07 -0600

    nested genericrelation serializers not working in django 1.6

--- tests/testapp/tests.py
+++ tests/testapp/tests.py
@@ -10,6 +10,7 @@ class TestCase(APITestCase):
         instance.description = "Test"
         instance.save()
 
+    # Test url="" use case
     def test_list_at_root(self):
         response = self.client.get("/.json")
         self.assertTrue(status.is_success(response.status_code))
@@ -19,3 +20,14 @@ class TestCase(APITestCase):
         response = self.client.get('/instance.json')
         self.assertTrue(status.is_success(response.status_code))
         self.assertTrue(response.data['description'] == "Test")
+
+    # Ensure nested serializers are created for SerializableGenericRelations
+    # (e.g. identifiers), but only for detail views
+    def test_detail_nested_identifiers(self):
+        response = self.client.get('/instance.json')
+        self.assertIn('identifiers', response.data)
+        self.assertEqual(response.data['identifiers'][0]['slug'], 'instance')
+
+    def test_list_nested_identifiers(self):
+        response = self.client.get('/.json')
+        self.assertNotIn('identifiers', response.data['list'][0])
