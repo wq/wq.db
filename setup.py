94d15cc21e29e48f3a0e5ff595a4fa9b213649af
S. Andrew Sheppard <andrew@wq.io>
2014-02-08 17:39:42 -0600

    v0.5.0

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ if len(sys.argv) > 1 and sys.argv[1] == "test":
 
 setup(
     name='wq.db',
-    version='0.5.0-dev',
+    version='0.5.0',
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='http://wq.io/wq.db',
