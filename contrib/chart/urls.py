c053fa8c07f8702e40926036f45e497343d818b7
S. Andrew Sheppard <andrew@wq.io>
2014-05-22 20:06:44 -0500

    add ScatterView to urls

--- contrib/chart/urls.py
+++ contrib/chart/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import TimeSeriesView, BoxPlotView
+from .views import TimeSeriesView, ScatterView, BoxPlotView
 
 ids = r'^(?P<ids>[^\.]+)'
 
 urlpatterns = patterns('',
     url(ids + r'/timeseries$', TimeSeriesView.as_view()),
+    url(ids + r'/scatter$', ScatterView.as_view()),
     url(ids + r'/boxplot$', BoxPlotView.as_view()),
 )
 urlpatterns = format_suffix_patterns(urlpatterns)
