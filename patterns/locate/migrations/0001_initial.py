612447bd97e8d5ea16a346c5f3b4ec20f6538d16
S. Andrew Sheppard <andrew@wq.io>
2014-09-19 18:19:50 +0000

    use settings.SRID

--- patterns/locate/migrations/0001_initial.py
+++ patterns/locate/migrations/0001_initial.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import django.contrib.gis.db.models.fields
 
+from django.conf import settings
+
 
 class Migration(migrations.Migration):
 
@@ -18,7 +20,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('name', models.CharField(max_length=255, blank=True, null=True)),
                 ('is_primary', models.BooleanField(default=False)),
-                ('geometry', django.contrib.gis.db.models.fields.GeometryField(srid=4326)),
+                ('geometry', django.contrib.gis.db.models.fields.GeometryField(srid=settings.SRID)),
                 ('accuracy', models.IntegerField(blank=True, null=True)),
                 ('object_id', models.PositiveIntegerField()),
                 ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
