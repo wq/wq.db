2e22859104c1771e862133b8ab38e63afacbd495
S. Andrew Sheppard <andrew@wq.io>
2014-09-19 18:10:35 +0000

    no-op migration to keep django 1.7 happy

---
+++ rest/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ContentType',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('contenttypes.contenttype',),
+        ),
+    ]
