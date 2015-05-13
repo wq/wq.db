46b2e4d33fcee9d410af10e6972fea474ca13873
S. Andrew Sheppard <andrew@wq.io>
2015-05-13 12:59:16 -0500

    option to dump AMD-wrapped config file

--- rest/management/commands/dump_config.py
+++ rest/management/commands/dump_config.py
@@ -1,14 +1,22 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from wq.db import rest
 import json
+from optparse import make_option
 
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
+class Command(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.option_list += (make_option(
+            '--format', default='json'
+        ),)
+
+    def handle(self, *args, **options):
         rest.autodiscover()
-        self.stdout.write(
-            json.dumps(
-                rest.router.get_config(),
-                indent=4,
-            )
+        text = json.dumps(
+            rest.router.get_config(),
+            indent=4,
         )
+        if options['format'] == "amd":
+            text = "define(%s);" % text
+        self.stdout.write(text)
---
+++ tests/test_commands.py
@@ -0,0 +1,34 @@
+from rest_framework.test import APITestCase
+from django.core.management import call_command
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+import json
+
+
+class CommandTestCase(APITestCase):
+    def check_config(self, text):
+        data = json.loads(text)
+        self.assertIn('pages', data)
+        page = list(data['pages'].values())[0]
+        self.assertIn('url', page)
+
+    def test_dump_config_json(self):
+        f = StringIO()
+        call_command('dump_config', stdout=f)
+        self.check_config(f.getvalue())
+
+    def test_dump_config_amd(self):
+        f = StringIO()
+        call_command('dump_config', format='amd', stdout=f)
+        text = f.getvalue().strip()
+        self.assertTrue(
+            text.startswith('define('), "Unexpected start: %s..." % text[:10]
+        )
+        self.assertTrue(
+            text.endswith(');'), "Unexpected end: ...%s" % text[-10:]
+        )
+        text = text.replace('define(', '')
+        text = text.replace(');', '')
+        self.check_config(text)
