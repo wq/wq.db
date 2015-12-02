e8cec02d76cdc00227f6c48c53ce41505e029bb6
S. Andrew Sheppard <andrew@wq.io>
2015-12-02 16:44:01 +0000

    include matching attachment in result

--- contrib/search/serializers.py
+++ contrib/search/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework.serializers import Serializer
 from wq.db.rest.models import get_ct, ContentType, get_object_id
+from wq.db.patterns.models import Identifier, Annotation
 
 
 class SearchResultSerializer(Serializer):
     def to_representation(self, obj):
         if hasattr(obj, 'content_type_id') and hasattr(obj, 'content_object'):
             ctype = ContentType.objects.get(pk=obj.content_type_id)
+            if isinstance(obj, Identifier):
+                match_str = "%s (%s)" % (obj.name, obj.slug)
+            elif isinstance(obj, Annotation):
+                match_str = obj.value
+            else:
+                match_str = str(obj)
             obj = obj.content_object
         else:
             ctype = get_ct(obj)
+            match_str = str(obj)
 
         obj_id = get_object_id(obj)
 
@@ -21,5 +29,6 @@ class SearchResultSerializer(Serializer):
             'id': obj_id,
             'url': url,
             'type': str(ctype),
-            'label': str(obj)
+            'label': str(obj),
+            'match': match_str,
         }
