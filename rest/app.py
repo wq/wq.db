3bc4a0af7f12bcc5eb2445600d75fd9a4accc84d
S. Andrew Sheppard <andrew@wq.io>
2016-02-22 22:31:31 -0500

    remove obsolete modules

--- rest/app.py
+++
@@ -1,9 +0,0 @@
-import warnings
-warnings.warn(
-    "The wq.db.rest.app API has been moved to wq.db.rest. "
-    "The actual Router implementation has moved to wq.db.rest.routers",
-    DeprecationWarning
-)
-
-
-from . import ModelRouter as Router, router, autodiscover  # NOQA
--- rest/settings.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ImproperlyConfigured
-
-raise ImproperlyConfigured("""
-wq.db.rest.settings has been renamed to wq.db.default_settings.
-Please update your configuration.
-See https://wq.io/wq.db/releases/v0.8.0 for more information.
-""")
