b57d8733a32128153f73d1968f09da9be3c147fc
S. Andrew Sheppard <andrew@wq.io>
2019-03-21 16:47:28 +0900

    fix django 1.11 tests

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,20 @@
-from django.urls import include, path
+try:
+    from django.urls import include, path
+except ImportError:
+    from django.conf.urls import include, url
+    path = None
+
 from wq.db import rest
 from tests.patterns_app.views import FilterableView
 
 
-urlpatterns = [
-    path('filterable/<path:ids>', FilterableView.as_view()),
-    path('', include(rest.router.urls)),
-]
+if path:
+    urlpatterns = [
+        path('filterable/<path:ids>', FilterableView.as_view()),
+        path('', include(rest.router.urls)),
+    ]
+else:
+    urlpatterns = [
+        url(r'^filterable/(?P<ids>.+)', FilterableView.as_view()),
+        url(r'^', include(rest.router.urls)),
+    ]
