f4ba9375ad513222006275d967a1a8871ac4cd36
S. Andrew Sheppard <andrew@wq.io>
2020-09-02 13:54:27 +0900

    no more contrib

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
 set -e
 if [ "$LINT" ]; then
-    flake8 rest patterns contrib tests --exclude migrations
+    flake8 rest patterns tests --exclude migrations
     flake8 */*/migrations --ignore E501
 else
     python setup.py test
