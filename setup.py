43a9cc69bbc53f472a307ad4bcd3d7f908942906
Tomasz Nowak <tomaszn@users.noreply.github.com>
2021-04-12 17:53:09 +0200

    make compatible with DRF 1.12 working with Django 3.1

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         'Django>=1.11,<4.0',
-        'djangorestframework>=3.8.0,<3.12',
+        'djangorestframework>=3.8.0,<3.13',
         'django-mustache',
         'html-json-forms',
         'natural-keys>=1.6.0',
